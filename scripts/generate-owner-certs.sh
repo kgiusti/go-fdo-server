132b24c10a18d7b4537b2cb1c7f28fd9d81017cf
Kenneth Giusti <kgiusti@redhat.com>
2025-11-13 16:55:45 -0500

    chore: remove sysconfig files and refactor key management
    Remove sysconfig files in favor of systemd drop-ins.
    
    Refactor default certificate and key generation. Establish
    /etc/pki/go-fdo-server as the default directory for storing
    keys. Trigger key generation via a one-shot systemd service. Update
    tests accordingly. Install certificate tooling to private /usr/libexec
    directory.
    
    Correct minor rpmlint errors.
    
    Fixes: #107
    Signed-off-by: Kenneth Giusti <kgiusti@redhat.com>

--- scripts/generate-owner-certs.sh
+++ scripts/generate-owner-certs.sh
@@ -2,14 +2,9 @@
 
 source "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/cert-utils.sh"
 
-ENV_FILE="/etc/sysconfig/go-fdo-server-owner"
-[ ! -f "${ENV_FILE}" ] || source "${ENV_FILE}"
-
-conf_dir="${OWNER_CONF_DIR:-/etc/go-fdo-server}"
-subj="${OWNER_SUBJECT:-/C=US/O=FDO/CN=Owner}"
-key="${OWNER_KEY:-${conf_dir}/owner.key}"
-crt="${OWNER_CRT:-${conf_dir}/owner.crt}"
+cert_dir="/etc/pki/go-fdo-server"
+subj="/C=US/O=FDO/CN=Owner"
+key="${cert_dir}/owner.key"
+crt="${cert_dir}/owner.crt"
 
 generate_cert "${key}" "${crt}" "${subj}"
-
-"$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/generate-device-ca-certs.sh"
