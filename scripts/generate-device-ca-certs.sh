8bc0c622e6596f630eab35410532a83add9d77a5
Kenneth Giusti <kgiusti@redhat.com>
2025-11-13 16:55:45 -0500

    chore: remove sysconfig files and refactor key management
    Remove sysconfig files in favor of systemd drop-ins.
    
    Refactor default certificate and key generation. Establish
    /etc/pki/go-fdo-server as the default directory for storing
    keys. Trigger key generation via a one-shot systemd service. Update
    tests accordingly. Install certificate tooling to private /usr/libexec
    directory.
    
    Correct minor rpmlint errors.
    
    Fixes: #107
    Signed-off-by: Kenneth Giusti <kgiusti@redhat.com>

--- scripts/generate-device-ca-certs.sh
+++ scripts/generate-device-ca-certs.sh
@@ -2,12 +2,10 @@
 
 source "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/cert-utils.sh"
 
-ENV_FILE="/etc/sysconfig/go-fdo-server-device-ca"
-[ ! -f "${ENV_FILE}" ] || source "${ENV_FILE}"
-
-conf_dir="${DEVICE_CA_CONF_DIR:-/etc/go-fdo-server}"
-subj="${DEVICE_CA_SUBJECT:-/C=US/O=FDO/CN=Device CA}"
-key="${DEVICE_CA_KEY:-${conf_dir}/device-ca.key}"
-crt="${DEVICE_CA_CRT:-${conf_dir}/device-ca.crt}"
+cert_dir="/etc/pki/go-fdo-server"
+subj="/C=US/O=FDO/CN=Device CA"
+key="${cert_dir}/device-ca.key"
+crt="${cert_dir}/device-ca.crt"
+pub="${cert_dir}/device-ca.pub"
 
 generate_cert "${key}" "${crt}" "${subj}"
