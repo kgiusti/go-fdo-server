694fd21a3f205abe8db2af537bb6cf610e73ca2c
Kenneth Giusti <kgiusti@redhat.com>
2025-11-21 12:16:50 -0500

    ci: add config file tooling to fmf test
    Manage scratch home directories for per-test configuration files and
    state
    
    Signed-off-by: Kenneth Giusti <kgiusti@redhat.com>

--- test/rpm/test-onboarding-https.sh
+++ test/rpm/test-onboarding-https.sh
@@ -16,6 +16,36 @@ owner_protocol=https
 owner_url="${owner_protocol}://${owner_service}"
 owner_health_url="${owner_url}/health"
 
+# Enable HTTPS by including https certs/keys in the server's
+# configuration file.
+
+configure_service_manufacturer() {
+  log_info "Generating manufacturer HTTPS configuration"
+  sudo cp "${manufacturer_https_key}" "${rpm_manufacturer_https_key}"
+  sudo cp "${manufacturer_https_crt}" "${rpm_manufacturer_https_crt}"
+  sudo chown ${rpm_manufacturer_user}:${rpm_group} "${rpm_manufacturer_https_key}" "${rpm_manufacturer_https_crt}"
+  generate_manufacturer_config
+}
+
+configure_service_rendezvous() {
+  log_info "Generating rendezvous HTTPS configuration"
+  sudo cp "${rendezvous_https_key}" "${rpm_rendezvous_https_key}"
+  sudo cp "${rendezvous_https_crt}" "${rpm_rendezvous_https_crt}"
+  sudo chown ${rpm_rendezvous_user}:${rpm_group} "${rpm_rendezvous_https_key}" "${rpm_rendezvous_https_crt}"
+  generate_rendezvous_config
+}
+
+configure_service_owner() {
+  log_info "Generating owner HTTPS configuration"
+  if [ "${rendezvous_protocol}" = "https" ]; then
+    rpm_owner_to0_insecure_tls="true"
+  fi
+  sudo cp "${owner_https_key}" "${rpm_owner_https_key}"
+  sudo cp "${owner_https_crt}" "${rpm_owner_https_crt}"
+  sudo chown ${rpm_owner_user}:${rpm_group} "${rpm_owner_https_key}" "${rpm_owner_https_crt}"
+  generate_owner_config
+}
+
 # Allow running directly
 [[ "${BASH_SOURCE[0]}" != "$0" ]] || { run_test; cleanup; }
 
